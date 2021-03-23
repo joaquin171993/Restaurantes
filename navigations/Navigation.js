87940082a938ec795884414f7dcfca822806fe78
Joaquin Salas <joaquinsalas-1993@hotmail.com>
2021-03-23 17:24:36 -0600

    Update 23-3-2021 5:24 pm

--- navigations/Navigation.js
+++ navigations/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Icon } from 'react-native-elements'
 
 import RestaurantsStack from '../navigations/RestaurantsStack'
 import AccountStack from './AccountStack';
@@ -11,9 +12,50 @@ import SearchStack from './SearchStack';
 const Tab = createBottomTabNavigator()
 
 export default function Navigation(){
+
+    const screenOptions = (route, color) => {
+        let iconName = "";
+
+        switch(route.name){
+            case "restaurants":
+                iconName = "compass-outline";
+                break;
+            case "favorites":
+                iconName = "heart-outline";
+                break;
+            case "toprestaurants":
+                iconName = "star-outline";
+                break;
+            case "account":
+                iconName = "home-outline";
+                break;
+            case "search":
+                iconName = "magnify";
+                break;
+        }
+
+        return (
+            <Icon 
+                type="material-community"
+                name = {iconName}
+                size={22}
+                color = {color}
+            />
+        )
+    }
+
     return ( 
         <NavigationContainer>
-            <Tab.Navigator>
+            <Tab.Navigator 
+                initialRouteName = "restaurants"
+                tabBarOptions = {{
+                    inactiveTintColor : "#2c2434",
+                    activeTintColor: "#821c1c"
+                }}      
+                screenOptions={({ route }) => ({
+                    tabBarIcon : ({ color }) => screenOptions(route, color)
+                })}      
+            >
                 <Tab.Screen 
                     name="restaurants"
                     component={RestaurantsStack}
